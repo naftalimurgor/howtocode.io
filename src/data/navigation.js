f13565c4cfeb78066b55a669ba3f0d2776b6ba0e
Robert Guss <robertguss@pm.me>
2022-10-10 13:12:40 -0400

    added all articles to navigation

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -56,28 +56,55 @@ export const navigation = [
     ],
   },
   {
-    title: 'Misc.',
+    title: 'Astro (work in progress)',
+    links: [
+      {
+        title: '1. Overview & Intro',
+        href: '/posts/astro-static-site-generator-overview',
+      },
+    ],
+  },
+  {
+    title: 'Screencasts',
     links: [
       {
         title: 'How to Create Developer Screencasts with Keynote',
         href: '/posts/how-to-create-developer-screencasts-with-keynote',
       },
+    ],
+  },
+  {
+    title: 'Writing & PKM',
+    links: [
       {
         title: 'What is Zettelkasten?',
         href: '/posts/what-is-zettelkasten',
       },
       {
-        title:
-          'How to manage your expectations while teaching yourself how to code',
-        href: '/posts/how-to-manage-your-expectations-while-teaching-yourself-how-to-code',
-      },
-      {
         title: 'Why Writing is Important for Software Developers',
         href: '/posts/why-writing-is-important-for-software-developers',
       },
+    ],
+  },
+  {
+    title: 'Career Advice',
+    links: [
       {
         title: 'How I became a web developer in 5 months',
         href: '/posts/how-i-taught-myself-how-to-code',
+      },
+      {
+        title: 'How to become a Front-End developer',
+        href: 'how-to-become-a-front-end-developer',
+      },
+      {
+        title: 'How to become a Back-End developer',
+        href: 'how-to-become-a-back-end-developer',
+      },
+      {
+        title:
+          'How to manage your expectations while teaching yourself how to code',
+        href: '/posts/how-to-manage-your-expectations-while-teaching-yourself-how-to-code',
       },
     ],
   },
@@ -95,6 +122,164 @@ export const navigation = [
       {
         title: 'Blog with Hugo, Netlify CMS and Zapier',
         href: '/posts/jamstack-hugo-netlify-zapier',
+      },
+    ],
+  },
+  {
+    title: 'AdonisJS',
+    links: [
+      {
+        title: '1. Intro and Setup',
+        href: '/posts/adonis-js-1-intro-and-setup',
+      },
+      {
+        title: '2. Routes, Layouts, Controllers',
+        href: '/posts/adonis-js-2-routes-layouts',
+      },
+      {
+        title: '3. Migrations, Models, Forms',
+        href: '/posts/adonis-js-3-migrations-models',
+      },
+      {
+        title: '4. Seeds & Factories',
+        href: '/posts/adonis-js-4-seeds-factories',
+      },
+      {
+        title: '5. User Authentication',
+        href: '/posts/adonis-js-5-user-authentication',
+      },
+    ],
+  },
+  {
+    title: 'Tailwind CSS',
+    links: [
+      {
+        title: '1. Installing Tailwind CSS',
+        href: '/posts/tailwind-css-travel-site-1-installing-tailwind-css',
+      },
+      {
+        title: '2. Navigation & Hero',
+        href: '/posts/tailwind-css-travel-site-2-navigation-and-hero',
+      },
+      {
+        title: '3. Quote Form',
+        href: '/posts/tailwind-css-travel-site-3-quote-form',
+      },
+      {
+        title: '4. Popular Destinations',
+        href: '/posts/tailwind-css-travel-site-4-popular-destinations',
+      },
+      {
+        title: '5. Features Section',
+        href: '/posts/tailwind-css-travel-site-5-features-section',
+      },
+      {
+        title: '6. Callouts Section',
+        href: '/posts/tailwind-css-travel-site-6-callouts-section',
+      },
+      {
+        title: '7. Testimonial Section',
+        href: '/posts/tailwind-css-travel-site-7-testimonial-section',
+      },
+      {
+        title: '8. Footer Section & PurgeCSS',
+        href: '/posts/tailwind-css-travel-site-8-footer-purgecss',
+      },
+    ],
+  },
+  {
+    title: 'Go',
+    links: [
+      {
+        title: '1. Installing Go on a Mac',
+        href: '/posts/go-basics-1-installing-go-on-a-mac',
+      },
+      {
+        title: '2. Variables',
+        href: '/posts/go-basics-2-variables-in-go',
+      },
+      {
+        title: '3. Primitives',
+        href: '/posts/go-basics-3-primitives-in-go',
+      },
+      {
+        title: '4. Constants',
+        href: '/posts/go-basics-4-constants-in-go',
+      },
+      {
+        title: '5. Arrays & Slices',
+        href: '/posts/go-basics-5-arrays-and-slices-in-go',
+      },
+      {
+        title: '6. Maps',
+        href: '/posts/go-basics-6-maps-in-go',
+      },
+      {
+        title: '7. Structs',
+        href: '/posts/go-basics-7-structs-in-go',
+      },
+      {
+        title: '8. Control flow',
+        href: '/posts/go-basics-8-control-flow-in-go',
+      },
+      {
+        title: '9. Loops',
+        href: '/posts/go-basics-9-loops-in-go',
+      },
+      {
+        title: '10. Pointers',
+        href: '/posts/go-basics-10-pointers-in-go',
+      },
+      {
+        title: '11. Functions',
+        href: '/posts/go-basics-11-functions-in-go',
+      },
+    ],
+  },
+  {
+    title: 'Statamic',
+    links: [
+      {
+        title: 'How to deploy Statamic 3 to Digitalocean App platform',
+        href: '/posts/how-to-deploy-statamic-3-to-digitalocean-app-platform',
+      },
+    ],
+  },
+  {
+    title: 'Hugo',
+    links: [
+      {
+        title: 'How to customize your sitemap in Hugo',
+        href: 'how-to-customize-your-sitemap-in-hugo',
+      },
+    ],
+  },
+  {
+    title: 'JavaScript',
+    links: [
+      {
+        title: 'Scope and Closures',
+        href: '/posts/understanding-closures-in-javascript',
+      },
+      {
+        title: 'Looping through objects',
+        href: '/posts/looping-through-objects-in-javascript',
+      },
+      {
+        title: 'Hoisting',
+        href: '/posts/understanding-hoisting-in-javascript',
+      },
+      {
+        title: 'How to console log better',
+        href: '/posts/how-to-console-log-better-in-javascript',
+      },
+      {
+        title: 'How to compare dates in JavaScript',
+        href: '/posts/how-to-compare-dates-in-javascript',
+      },
+      {
+        title: 'Map, Filter & Reduce',
+        href: '/posts/understanding-map-filter-and-reduce-in-javascript',
       },
     ],
   },
