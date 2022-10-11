bba0e64b7952cfd1d2bbb9093d3195ed26403eb3
Robert Guss <robertguss@pm.me>
2022-10-11 09:34:45 -0400

    added google analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -44,6 +44,22 @@ export default function Document() {
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
