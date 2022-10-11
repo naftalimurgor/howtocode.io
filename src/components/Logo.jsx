aeff8c925539e3616ebc3c29671b019d5c0e025f
Robert Guss <robertguss@pm.me>
2022-10-11 08:28:40 -0400

    fixed logo on mobile and dark mode

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -27,14 +27,8 @@ export function Logomark(props) {
 
 export function Logo(props) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="h-6 w-6"
-    >
+    <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" {...props}>
+      <LogomarkPaths />
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
